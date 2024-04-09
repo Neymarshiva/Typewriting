1f5980f1b6570f4a058a0bb5795355662bf3c435
SIVAKUMAR SUGENDRAN <t474848@deluxe.com>
2024-04-09 11:06:11 +0530

    changes for color changes for darkmode

--- src/Web/ClientApp/src/ui/CountrySelector.jsx
+++ src/Web/ClientApp/src/ui/CountrySelector.jsx
@@ -8,8 +8,10 @@ const StyledCountryDiv = styled.div`
 
   .menu-flags #rfs-btn span {
     overflow: visible !important;
+    color:var(--color-grey-600);
   }
 
+  
   & ul {
     margin-top:0.5rem !important;
     font-size: 16px;
@@ -24,11 +26,25 @@ const StyledCountryDiv = styled.div`
     max-height: 450px;
     border-radius: 1rem !important;
     border: 1px solid rgba(0, 0, 0, 0.15);
+    background:var(--color-grey-0);
+
+    & div {
+      background:var(--color-grey-0);
+    }
+
+    & div input{
+      background:var(--color-grey-0);
+    }
 
     & li {
       font-size: 16px;
       padding: 0.4rem 1.5rem;
-      border-bottom: 1px dashed #eee;
+      border-bottom: 1px dashed var(--color-grey-100);
+    }
+    & li:hover{
+      color: var(--color-grey-800);
+      background-color: var(--color-grey-50);
+      border-radius: var(--border-radius-sm);
     }
   }
 
@@ -36,23 +52,23 @@ const StyledCountryDiv = styled.div`
 `;
 
 function CountrySelector() {
-    const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState("");
 
 
 
 
-    return (
-        <StyledCountryDiv>
-            <ReactFlagsSelect
-                selected={selected}
-                onSelect={(code) => setSelected(code)}
-                searchable
-                searchPlaceholder="Search countries"
-                className="menu-flags"
-            />
-        </StyledCountryDiv>
+  return (
+    <StyledCountryDiv>
+      <ReactFlagsSelect
+        selected={selected}
+        onSelect={(code) => setSelected(code)}
+        searchable
+        searchPlaceholder="Search countries"
+        className="menu-flags"
+      />
+    </StyledCountryDiv>
 
-    );
+  );
 }
 
 export default CountrySelector
